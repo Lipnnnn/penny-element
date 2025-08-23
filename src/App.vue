093e7658004408d234828244d652f5bdc8ef0bc6
李佩妮 <lipn2002@163.com>
2025-08-23 14:25:12 +0800

    完成Button组件

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import Button from "./components/Button/Button.vue";
+import { onMounted, ref } from "vue";
+import type { ButtonInstance } from "./components/Button/types";
+
+const buttonRef = ref<ButtonInstance>();
+
+onMounted(() => {
+  console.log(buttonRef.value?.ref);
+});
+</script>
+
+<template>
+  <Button ref="buttonRef">Test Button</Button>
+  <Button plain>Plain Button</Button>
+  <Button round>Round Button</Button>
+  <Button circle>VK</Button>
+  <Button disabled>Disabled Button</Button><br /><br />
+  <Button type="primary">Primary</Button>
+  <Button type="success">Success</Button>
+  <Button type="info">Info</Button>
+  <Button type="warning">Warning</Button>
+  <Button type="danger">Danger</Button><br /><br />
+  <Button type="primary" plain>Primary</Button>
+  <Button type="success" plain>Success</Button>
+  <Button type="info" plain>Info</Button>
+  <Button type="warning" plain>Warning</Button>
+  <Button type="danger" plain>Danger</Button><br /><br />
+  <Button size="large">Large</Button>
+  <Button size="small">Small</Button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    ref="_ref"
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-disabled': disabled,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+    }"
+    :disabled="disabled"
+    :type="nativeType"
+    :autofocus="autofocus"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonType, ButtonSize, NativeType } from "./types";
+import { ref } from "vue";
+
+withDefaults(
+  defineProps<{
+    type?: ButtonType;
+    size?: ButtonSize;
+    disabled?: boolean;
+    plain?: boolean;
+    round?: boolean;
+    circle?: boolean;
+    nativeType?: NativeType;
+    autofocus?: boolean;
+  }>(),
+  {
+    nativeType: "button",
+  }
+);
+
+const _ref = ref<HTMLButtonElement>()
+
+defineExpose({
+  ref: _ref
+})
+</script>
+
